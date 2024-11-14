a886500cca321b18b612a79613f2a8232ec87b64
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-11-14 15:48:30 -0500

    completed exercise

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,19 @@
 function reverseString(str) {
+  let newString ="";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString
+
   // type your code here
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("expecting: tac")
+  console.log("=>", reverseString("cat"));
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
